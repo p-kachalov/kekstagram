e7b6607cba8b6bc6e08202d593ab6cb205e95c9e
Pavel K <p.kachalov@yandex.ru>
2018-04-16 11:41:49 +0300

    upload and show

--- js/picture.js
+++ js/picture.js
@@ -7,6 +7,11 @@
   var LIKES_MAX = 200;
   var PHOTOS_PATH = 'photos/';
   var PHOTOS_EXT = '.jpg';
+  var ESC_KEYCODE = 27;
+  var RESIZE_STEP = 25;
+  var RESIZE_VALUE_DEFAULT = 100;
+  var RESIZE_VALUE_MIN = 25;
+  var RESIZE_VALUE_MAX = 100;
 
   var COMMENTS_CATALOG = [
     'Всё отлично!',
@@ -39,10 +44,18 @@
       ];
   };
 
+  var getPhotoByID = function (photoSet, id) {
+    var result = photoSet.filter(function (item) {
+      return item.id === id;
+    });
+    return result[0] ? result[0] : null;
+  };
+
   var makeRandomPhotosSet = function (num) {
     var photosList = [];
     for (var i = 1; i <= num; i++) {
       var photo = {
+        id: i,
         url: PHOTOS_PATH + i + PHOTOS_EXT,
         likes: LIKES_MIN + getRandomInt(LIKES_MAX - LIKES_MIN + 1),
         comments: getRandomComments(),
@@ -75,6 +88,7 @@
     var pictureImage = pictureElement.querySelector('.picture__img');
     var pictureLikes = pictureElement.querySelector('.picture__stat--likes');
     var pictureComents = pictureElement.querySelector('.picture__stat--comments');
+    pictureElement.dataset.id = photo.id;
     pictureImage.src = photo.url;
     pictureLikes.textContent = photo.likes;
     pictureComents.textContent = photo.comments.length;
@@ -86,7 +100,9 @@
     var photoTemplate = document.querySelector('#picture')
         .content.querySelector('.picture__link');
     for (var i = 0; i < data.length; i++) {
-      fragment.appendChild(renderPictureElement(data[i], photoTemplate));
+      var pictureElement = renderPictureElement(data[i], photoTemplate);
+      pictureElement.addEventListener('click', onPictureElementClick);
+      fragment.appendChild(pictureElement);
     }
     return fragment;
   };
@@ -102,7 +118,6 @@
   };
 
   var showBigPictureElemnt = function (photo) {
-    var bigPictureElement = document.querySelector('.big-picture');
     var pictureImage = bigPictureElement.querySelector('.big-picture__img img');
     var pictureLikes = bigPictureElement.querySelector('.likes-count');
     var pictureComentsCount = bigPictureElement.querySelector('.comments-count');
@@ -127,7 +142,28 @@
     socialCommentLoad.classList.add('visually-hidden');
 
     bigPictureElement.classList.remove('hidden');
+    document.addEventListener('keydown', onDocumentBigPictureEscKeydown);
+  };
 
+  var hideBigPictureElemnt = function () {
+    bigPictureElement.classList.add('hidden');
+    document.removeEventListener('keydown', onDocumentBigPictureEscKeydown);
+  };
+
+  var onBigPictureCancelClick = function () {
+    hideBigPictureElemnt();
+  };
+
+  var onDocumentBigPictureEscKeydown = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      hideBigPictureElemnt();
+    }
+  };
+
+  var onPictureElementClick = function (evt) {
+    evt.preventDefault();
+    var pictureId = parseInt(evt.currentTarget.dataset.id, 10);
+    showBigPictureElemnt(getPhotoByID(randomPhotosSet, pictureId));
   };
 
   var randomPhotosSet = makeRandomPhotosSet(PHOTOS_NUMBER);
@@ -135,5 +171,156 @@
   var picturesElement = document.querySelector('.pictures');
   picturesElement.appendChild(photosSetFragment);
 
-  showBigPictureElemnt(randomPhotosSet[0]);
+  var bigPictureElement = document.querySelector('.big-picture');
+  var bigPictureCancel = bigPictureElement.querySelector('.big-picture__cancel');
+  bigPictureCancel.addEventListener('click', onBigPictureCancelClick);
+
+
+  // Upload Actions
+  var uploadFileElement = document.querySelector('#upload-file');
+  var uploadCancedElement = document.querySelector('#upload-cancel');
+  var imageUploadElement = document.querySelector('.img-upload__overlay');
+
+  var resizeControlsPanel = document.querySelector('.img-upload__resize');
+  var resizeControlMinus = document.querySelector('.resize__control--minus');
+  var resizeControlPlus = document.querySelector('.resize__control--plus');
+  var resizeControlValue = document.querySelector('.resize__control--value');
+  var imagePreviewElement = document.querySelector('.img-upload__preview');
+
+  var effectControls = document.querySelectorAll('.effects__radio');
+  var scalePinControl = document.querySelector('.scale__pin');
+  var scaleValueElement = document.querySelector('.scale__value');
+  var imagePreviewImage = document.querySelector('.img-upload__preview img');
+
+  var effectsMap = {
+    none: {
+      className: '',
+      calcFilterValue: function () {
+        return null;
+      }
+    },
+    chrome: {
+      className: 'effects__preview--chrome',
+      calcFilterValue: function (value) {
+        return 'grayscale(' + value / 100 + ')';
+      }
+    },
+    sepia: {
+      className: 'effects__preview--sepia',
+      calcFilterValue: function (value) {
+        return 'sepia(' + value / 100 + ')';
+      }
+    },
+    marvin: {
+      className: 'effects__preview--marvin',
+      calcFilterValue: function (value) {
+        return 'invert(' + value + '%)';
+      }
+    },
+    phobos: {
+      className: 'effects__preview--phobos',
+      calcFilterValue: function (value) {
+        return 'blur(' + value / 100 * 3 + 'px)';
+      }
+    },
+    heat: {
+      className: 'effects__preview--heat',
+      calcFilterValue: function (value) {
+        return 'brightness(' + (1 + value / 100 * 2) + ')';
+      }
+    }
+  };
+
+  var showImageUploadElement = function () {
+    imageUploadElement.classList.remove('hidden');
+    document.addEventListener('keydown', onDocumentEscKeydown);
+
+    resizeControlsPanel.style.zIndex = 1;
+    resizeControlValue.value = RESIZE_VALUE_DEFAULT + '%';
+    applyEffect();
+  };
+
+  var hideImageUploadElement = function () {
+    imageUploadElement.classList.add('hidden');
+    document.removeEventListener('keydown', onDocumentEscKeydown);
+    uploadFileElement.value = '';
+  };
+
+  var applyEffect = function () {
+    var currentEffect = document.querySelector('.effects__radio:checked').value;
+    scaleValueElement.value = calcEffectScale();
+    scaleValueElement.value = 100;
+
+    imagePreviewImage.className = effectsMap[currentEffect].className;
+    imagePreviewImage.style.filter = effectsMap[currentEffect].calcFilterValue(scaleValueElement.value);
+  };
+
+  var decreaseResizeControlValue = function () {
+    var resizeValueNum = parseFloat(resizeControlValue.value);
+    resizeValueNum -= RESIZE_STEP;
+    if (resizeValueNum >= RESIZE_VALUE_MIN) {
+      resizeControlValue.value = resizeValueNum + '%';
+      imagePreviewElement.style.transform = 'scale(' + resizeValueNum / 100 + ')';
+    }
+  };
+
+  var increaseResizeControlValue = function () {
+    var resizeValueNum = parseFloat(resizeControlValue.value);
+    resizeValueNum += RESIZE_STEP;
+    if (resizeValueNum <= RESIZE_VALUE_MAX) {
+      resizeControlValue.value = resizeValueNum + '%';
+      imagePreviewElement.style.transform = 'scale(' + resizeValueNum / 100 + ')';
+    }
+  };
+
+  var calcEffectScale = function () {
+    var scaleLineElement = document.querySelector('.scale__line');
+    var scaleLevelElement = document.querySelector('.scale__level');
+    var maxValue = scaleLineElement.offsetWidth;
+    var currentValue = scaleLevelElement.offsetWidth;
+    return Math.round(currentValue / maxValue * 100);
+  };
+
+  var onUploadFileChange = function () {
+    showImageUploadElement();
+  };
+
+  var onUploadCancelClick = function () {
+    hideImageUploadElement();
+  };
+
+  var onDocumentEscKeydown = function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      hideImageUploadElement();
+    }
+  };
+
+  var onResizeControlMinusClick = function () {
+    decreaseResizeControlValue();
+  };
+
+  var onResizeControlPlusClick = function () {
+    increaseResizeControlValue();
+  };
+
+  var onEffectControlChange = function () {
+    applyEffect();
+  };
+
+  var onScalePinControlMouseup = function () {
+    applyEffect();
+  };
+
+  uploadFileElement.addEventListener('change', onUploadFileChange);
+  uploadCancedElement.addEventListener('click', onUploadCancelClick);
+
+  resizeControlMinus.addEventListener('click', onResizeControlMinusClick);
+  resizeControlPlus.addEventListener('click', onResizeControlPlusClick);
+
+  scalePinControl.addEventListener('mouseup', onScalePinControlMouseup);
+
+  for (var i = 0; i < effectControls.length; i++) {
+    effectControls[i].addEventListener('change', onEffectControlChange);
+  }
+
 })();
