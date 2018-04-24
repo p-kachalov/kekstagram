9cf5222f41880a4e8f524536a0c4807f7de939f6
Pavel K <p.kachalov@yandex.ru>
2018-04-24 17:14:44 +0300

    encapsulate effects

--- js/form.js
+++ js/form.js
@@ -7,97 +7,22 @@
   var uploadCancedElement = document.querySelector('#upload-cancel');
   var imageUploadElement = document.querySelector('.img-upload__overlay');
 
-  var effectControls = document.querySelectorAll('.effects__radio');
-  var scaleElement = document.querySelector('.img-upload__scale');
-  var scaleLineElement = document.querySelector('.scale__line');
-  var scaleLevelElement = document.querySelector('.scale__level');
-  var scalePinControl = document.querySelector('.scale__pin');
-  var scaleValueElement = document.querySelector('.scale__value');
-  var imagePreviewImage = document.querySelector('.img-upload__preview img');
 
   var hashTagsElement = document.querySelector('.text__hashtags');
   var imageUploadForm = document.querySelector('.img-upload__form');
-
-  var effectsMap = {
-    none: {
-      className: '',
-      calcFilterValue: function () {
-        return null;
-      }
-    },
-    chrome: {
-      className: 'effects__preview--chrome',
-      calcFilterValue: function (value) {
-        return 'grayscale(' + value / 100 + ')';
-      }
-    },
-    sepia: {
-      className: 'effects__preview--sepia',
-      calcFilterValue: function (value) {
-        return 'sepia(' + value / 100 + ')';
-      }
-    },
-    marvin: {
-      className: 'effects__preview--marvin',
-      calcFilterValue: function (value) {
-        return 'invert(' + value + '%)';
-      }
-    },
-    phobos: {
-      className: 'effects__preview--phobos',
-      calcFilterValue: function (value) {
-        return 'blur(' + value / 100 * 3 + 'px)';
-      }
-    },
-    heat: {
-      className: 'effects__preview--heat',
-      calcFilterValue: function (value) {
-        return 'brightness(' + (1 + value / 100 * 2) + ')';
-      }
-    }
-  };
 
   var showImageUploadElement = function () {
     imageUploadElement.classList.remove('hidden');
     document.addEventListener('keydown', onDocumentEscKeydown);
 
     window.resize.setDefaultSize();
-    applyEffect(true);
+    window.effects.applyEffect(true);
   };
 
   var hideImageUploadElement = function () {
     imageUploadElement.classList.add('hidden');
     document.removeEventListener('keydown', onDocumentEscKeydown);
     uploadFileElement.value = '';
-  };
-
-  var applyEffect = function (toDefault) {
-    var currentEffect = document.querySelector('.effects__radio:checked').value;
-
-    if (currentEffect === 'none') {
-      scaleElement.classList.add('hidden');
-    } else if (scaleElement.classList.contains('hidden')) {
-      scaleElement.classList.remove('hidden');
-    }
-
-    if (toDefault) {
-      var leftPositon = scaleLineElement.getBoundingClientRect().left;
-      var rightPositon = scaleLineElement.getBoundingClientRect().right;
-
-      scalePinControl.style.left = rightPositon - leftPositon + 'px';
-      scaleLevelElement.style.width = rightPositon - leftPositon + 'px';
-      scaleValueElement.value = calcEffectScale();
-    }
-
-    imagePreviewImage.className = effectsMap[currentEffect].className;
-    imagePreviewImage.style.filter = effectsMap[currentEffect].calcFilterValue(scaleValueElement.value);
-  };
-
-
-  var calcEffectScale = function () {
-    var maxValue = scaleLineElement.offsetWidth;
-    var currentValue = scaleLevelElement.offsetWidth;
-    return Math.round(currentValue / maxValue * 100);
   };
 
   var moreThanOnce = function (list, item) {
@@ -144,50 +69,6 @@
     }
   };
 
-  var onEffectControlChange = function () {
-    applyEffect(true);
-  };
-
-  var onScalePinControlMousedown = function (evt) {
-    evt.preventDefault();
-
-    var startCoordX = evt.clientX;
-    var linePosition = scaleLineElement.getBoundingClientRect();
-    var leftBorder = linePosition.left;
-    var rightBorder = linePosition.right;
-
-    var onScalePinControlMousemove = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      if (moveEvt.clientX < leftBorder || moveEvt.clientX > rightBorder) {
-        return;
-      }
-
-      var shiftX = startCoordX - moveEvt.clientX;
-      startCoordX = moveEvt.clientX;
-
-      scalePinControl.style.left = (scalePinControl.offsetLeft - shiftX) + 'px';
-      scaleLevelElement.style.width = (scaleLevelElement.offsetWidth - shiftX) + 'px';
-
-      scaleValueElement.value = calcEffectScale();
-      // console.log(scaleValueElement.value);
-      applyEffect();
-    };
-
-    var onScalePinControlMouseup = function (upEvt) {
-      upEvt.preventDefault();
-
-      scaleValueElement.value = calcEffectScale();
-      applyEffect();
-
-      document.removeEventListener('mousemove', onScalePinControlMousemove);
-      document.removeEventListener('mouseup', onScalePinControlMouseup);
-    };
-
-    document.addEventListener('mousemove', onScalePinControlMousemove);
-    document.addEventListener('mouseup', onScalePinControlMouseup);
-  };
-
   var onHashtagsInput = function () {
     if (!isTagsStringValid(hashTagsElement.value)) {
       hashTagsElement.setCustomValidity('hashtags invalid');
@@ -207,12 +88,6 @@
 
   uploadFileElement.addEventListener('change', onUploadFileChange);
   uploadCancedElement.addEventListener('click', onUploadCancelClick);
-
-  scalePinControl.addEventListener('mousedown', onScalePinControlMousedown);
-
-  for (var i = 0; i < effectControls.length; i++) {
-    effectControls[i].addEventListener('change', onEffectControlChange);
-  }
 
   hashTagsElement.addEventListener('input', onHashtagsInput);
   imageUploadForm.addEventListener('submit', onSubmitImageUplodadForm);
