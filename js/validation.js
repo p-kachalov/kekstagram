31420a8758b5f41ee0cce14e01f799fc6a814c44
Pavel K <p.kachalov@yandex.ru>
2018-04-26 17:18:43 +0300

    regexp tag validation

--- js/validation.js
+++ js/validation.js
@@ -2,9 +2,7 @@
 
 (function () {
   var MAX_TAGS = 5;
-  var TAG_LEN_MIN = 2;
-  var TAG_LEN_MAX = 20;
-  var TAG_START_SYMBOL = '#';
+  var TAG_TEMPLATE = RegExp('^#([a-zA-Z0-9_]{1,19})$');
 
   var hashTagsElement = document.querySelector('.text__hashtags');
 
@@ -26,9 +24,7 @@
 
     return tags.reduce(function (acc, item) {
       if (
-        item[0] !== TAG_START_SYMBOL ||
-        item.length < TAG_LEN_MIN ||
-        item.length > TAG_LEN_MAX ||
+        !TAG_TEMPLATE.test(item) ||
         moreThanOnce(tags, item)) {
         return false;
       }
