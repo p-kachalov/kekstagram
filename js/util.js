182f1ebd7402200015987f0c4127942d59f7d2be
Pavel K <p.kachalov@yandex.ru>
2018-05-04 19:54:35 +0300

    encapsulate error message

--- js/util.js
+++ js/util.js
@@ -5,31 +5,6 @@
     return Math.floor(Math.random() * Math.floor(max));
   };
 
-  var showErrorMessage = function (err) {
-    var errorBlock = document.createElement('div');
-    errorBlock.classList.add('error-message');
-    errorBlock.style.background = 'rgba(255, 100, 100, 0.9)';
-    errorBlock.style.display = 'flex';
-    errorBlock.style.position = 'absolute';
-    errorBlock.style.zIndex = '100';
-    errorBlock.style.top = '0';
-    errorBlock.style.width = '100%';
-    var errorText = document.createElement('p');
-    errorText.style.color = 'white';
-    errorText.style.fontWeight = 'bold';
-    errorText.style.margin = '20px auto';
-    errorText.textContent = err;
-    errorBlock.appendChild(errorText);
-    document.querySelector('body').appendChild(errorBlock);
-
-    var hideErrorMessage = function () {
-      document.querySelector('.error-message').remove();
-      document.removeEventListener('click', hideErrorMessage);
-    };
-
-    document.addEventListener('click', hideErrorMessage);
-  };
-
   var shuffleArray = function (arr) {
     var result = [];
     while (arr.length > 0) {
@@ -47,7 +22,6 @@
 
   window.util = {
     getRandomInt: getRandomInt,
-    showErrorMessage: showErrorMessage,
     shuffleArray: shuffleArray,
     isMoreThanOnce: isMoreThanOnce
   };
